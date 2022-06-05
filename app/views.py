bef72317fb1e6e10b44368904449a41d7360b3d0
mavas <10604882+mavas@users.noreply.github.com>
2022-06-05 00:05:08 -0700

    Add home page form.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.core.management import call_command
 
+from app.forms import HomeForm
+
 
 def home(request):
-    return render(request, 'app/home.html')
+    c = dict()
+    if request.method == 'POST':
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            pass
+    c['form'] = HomeForm()
+    return render(request, 'app/home.html', c)
