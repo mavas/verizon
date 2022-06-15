dc25dda1a847e6924e1df2caaf6c25de22d17838
mavas <10604882+mavas@users.noreply.github.com>
2022-06-14 20:03:03 -0700

    Fix that.

--- work/verizon/app/validators.py
+++ work/verizon/app/validators.py
@@ -5,9 +5,11 @@ from django.core.exceptions import ValidationError
 
 def validate_video_url(u):
     idts = ['ghY9xrSJexk', 'QJHcWIXymHA']
+
     for idt in idts:
-        s = r'^https://www.youtube.com/watch?v=%s$', idt)
+        s = r'^https://www.youtube.com/watch?v=%s$' % idt
         m = re.match(s, u)
         if m:
             return
+
     raise ValidationError("Not a valid video URL.")
